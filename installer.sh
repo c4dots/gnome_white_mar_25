961c0b9431dbedb36f12bcb6b09ea1872d00e655
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-03-15 17:49:14 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Go into cache folder
-mkdir ~/.dotcache/
+mkdir ~/.dotcache/ &> /dev/null
 cd ~/.dotcache/
 
 # Clone repo
-git clone https://github.com/c4dots/gnome_white_mar_25/
+git clone https://github.com/c4dots/gnome_white_mar_25/ &> /dev/null
 cd gnome_white_mar_25
 
 # Run setup
