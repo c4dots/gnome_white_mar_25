91692fafab927ed7e9c7312f8163bbbf204716f1
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-04-17 11:29:31 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@
 # Go into cache folder
 sudo -S mkdir -p ~/.cache/.dotcache/ &> /dev/null
 cd ~/.cache/.dotcache/
+sudo chmod 777 -R ~/.cache/.dotcache/
 
 # Clone repo
 git clone https://github.com/c4dots/gnome_white_mar_25/ &> /dev/null
