09e1bfef703c6c6e94374ef9b36b423ce5c9d30a
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-04-17 11:37:09 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,6 +8,7 @@ sudo chmod 777 -R ~/.cache/.dotcache/
 # Clone repo
 sudo -S git clone https://github.com/c4dots/gnome_white_mar_25/ &> /dev/null
 cd gnome_white_mar_25
+sudo chmod 777 -R .
 
 # Run setup
 bash setup.sh "$@"
